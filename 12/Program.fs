cf9b28074f382d5c1e902767f6f2517bc925d7d2
lajonss <l3n1@dupaw.eu>
2021-12-17 06:22:22 +0100

    12 pt 2

--- 12/Program.fs
+++ 12/Program.fs
@@ -26,21 +26,37 @@ let printMap (map:Map<String,List<String>>) =
     for kvp in map do
         printfn "%s: %A" kvp.Key kvp.Value
 
-let canGoTo (path:Set<String>) (name:String) =
+let canGoTo (path:List<String>) (name:String) =
     let r = new Regex("[A-Z]")
     if r.IsMatch (name)
         then true
-        else not (path.Contains (name))
+        else not (List.contains name path)
 
-let pickPaths (map:Map<String,List<String>>) (name:String) (path:Set<String>) =
-    List.filter (canGoTo path) map[name]
+let doesNotRepeatSmallCave path =
+    let r = new Regex("[a-z]")
+    match path |> List.filter r.IsMatch
+               |> List.countBy id
+               |> List.map snd
+               |> List.tryFind (fun e -> e > 1) with
+        | None -> true
+        | _ -> false
 
-let rec doCountPaths (map:Map<String,List<String>>) (name:String) (path:Set<String>) =
+let canGoToWithOneRevisit (path:List<String>) (name:String) =
+    if canGoTo path name
+        then true
+        else match name with
+                | "start" -> false
+                | _ -> doesNotRepeatSmallCave path
+
+let pickPaths (map:Map<String,List<String>>) (name:String) (path:List<String>) reachabilityCondition =
+    List.filter (reachabilityCondition path) map[name]
+
+let rec doCountPaths (map:Map<String,List<String>>) (name:String) (path:List<String>) reachabilityCondition =
     match name with
         | "end" -> 1
-        | _ -> List.sum ([for next in pickPaths map name (path.Add name) -> doCountPaths map next (path.Add name)])
-let countPaths map =
-    doCountPaths map "start" Set[]
+        | _ -> List.sum ([for next in pickPaths map name (name :: path) reachabilityCondition -> doCountPaths map next (name :: path) reachabilityCondition])
+let countPaths map nextPicker =
+    doCountPaths map "start" [] nextPicker
 
 let paths = parseLines (Seq.toList (readlines ()))
-printfn "%d" (countPaths paths)
+printfn "%d" (countPaths paths canGoToWithOneRevisit)
